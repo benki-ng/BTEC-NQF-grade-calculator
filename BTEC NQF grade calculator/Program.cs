5a692f5fc32f71a5cea4734457a61fedaec7b30c
Ben King <Ben King@DESKTOP-SU79ACJ>
2018-01-18 14:55:58 +0000

    Add project files.

---
+++ BTEC NQF grade calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTEC_NQF_grade_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
